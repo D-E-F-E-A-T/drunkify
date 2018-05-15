91edde1758dcc1536392b000b876bd9ee338cea5
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-05-15 13:30:39 -0300

    Init

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+var btnMispell = document.getElementById('misspelify');
+btnMispell.addEventListener('click', function(e) {
+    var sentenceElem = document.getElementById('correct');
+    var sentence = sentenceElem.value;
+    var words = sentence.split(' ');
+    words.forEach(function(word, i) {
+        console.log(word + i)
+    });
+});
